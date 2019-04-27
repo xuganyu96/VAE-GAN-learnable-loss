e9cff71d9a36e391311a170c162751943d3aa399
Ubuntu <ubuntu@ip-172-31-19-65.us-west-2.compute.internal>
2019-04-27 02:15:32 +0000

    Automated report for DenseVAE on anime

--- train_DenseVAE_on_anime.py
+++ train_DenseVAE_on_anime.py
@@ -32,9 +32,12 @@ _, n_channels, width, height = train_features.shape
 
 # Instantiate the model, then build the trainer and 
 # initialize the parameters
-dense_vae = DenseVAE(n_latent = 3,
-                    n_hlayers = 5,
-                    n_hnodes = 400,
+n_latent = 5
+n_hlayers = 5
+n_hnodes = 400
+dense_vae = DenseVAE(n_latent = n_latent,
+                    n_hlayers = n_hlayers,
+                    n_hnodes = n_hnodes,
                     n_out_channels = n_channels,
                     out_width = width,
                     out_height = height)
@@ -43,9 +46,19 @@ trainer = gluon.Trainer(dense_vae.collect_params(),
                         'adam', 
                         {'learning_rate': .001})
 
+# Specify the directory to which validation images and training
+# report (with training errors and time for each epoch) will be
+# saved
+result_dir = './results/images/DenseVAE_on_anime/5_5_400_40/'
 
+# Open a file to write to for training reports
+readme = open(result_dir + 'README.md', 'w')
+readme.write('Number of latent variables \t' + str(n_latent) + '\n\n')
+readme.write('Number of hidden layers \t' + str(n_hlayers) + '\n\n')
+readme.write('Number of hidden nodes per layer \t' + str(n_hnodes) + '\n\n')
 
 n_epoch = 40
+readme.write('Number of epochs trained \t' + str(n_epoch) + '\n\n')
 for epoch in range(n_epoch):
     
     batch_losses = []
@@ -64,16 +77,26 @@ for epoch in range(n_epoch):
     epoch_stop_time = time.time()
     time_consumed = epoch_stop_time - epoch_start_time
     
-    print('Epoch{}, Training loss {:.10f}, Time used {:.2f}'.format(epoch, 
-                                                                   epoch_train_loss, 
-                                                                   time_consumed))
+    # Generate the epoch report, write it to the report file and print it
+    epoch_report_str = 'Epoch{}, Training loss {:.10f}, Time used {:.2f}'.format(epoch,
+                                                                                 epoch_train_loss,
+                                                                                 time_consumed)
+    readme.write(epoch_report_str + '\n\n')
+    print(epoch_report_str)
     
 # Validation
 img_arrays = dense_vae.generate(test_features).asnumpy()
 
-for i in range(10):
+# Define the number of validation images to generate (and display in the README.md)
+n_validations = 10
+
+for i in range(n_validations):
+    # Add a line that writes to the report to display the images
+    readme.write('!['+str(i)+'](./'+str(i)+'.png)')
     img_array = img_arrays[i]
     fig = plt.figure()
     plt.imshow(img_array.reshape(width, height, n_channels))
-    plt.savefig('./results/images/DenseVAE_on_anime/3_5_400_40/' + str(i) + '.png')
+    plt.savefig('./results/images/DenseVAE_on_anime/5_5_400_40/' + str(i) + '.png')
     plt.close()
+    
+readme.close()
